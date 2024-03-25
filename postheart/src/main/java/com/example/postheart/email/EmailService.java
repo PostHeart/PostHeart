84491f54085790b36078feffb1e37ecb48ce5c28
QuagmireFromGamGuy <leonardo11907vo@gmail.com>
2024-03-25 13:58:54 -0700

    email registration now sends email through smtp server

--- postheart/src/main/java/com/example/postheart/email/EmailService.java
+++ postheart/src/main/java/com/example/postheart/email/EmailService.java
@@ -1,32 +1,35 @@
 package com.example.postheart.email;
 
+
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
+
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 
+import lombok.AllArgsConstructor;
+
 @Service
+@AllArgsConstructor
 public class EmailService implements EmailSender {
 
     private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
 
-    private final JavaMailSender mailSender;
 
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
+    private final JavaMailSender mailSender;
 
     @Override
     @Async
     public void send(String to, String email) {
         try {
             MimeMessage mimeMessage = mailSender.createMimeMessage();
-            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true); // true for multipart
-            helper.setText(email, true); // true for HTML
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true); 
             helper.setTo(to);
             helper.setSubject("Confirm your email");
             helper.setFrom("cascadecalc@gmail.com");
